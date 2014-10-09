ed61c82fb4b8d002ff427922dda06d60517a0c03
James Work <james@teetimes4you.com>
2014-10-09 11:00:49 +0100

    Create project

---
+++ mysite/mysite/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+sys.path.append('/var/www/django/mysite/')
+sys.path.append('/var/www/django/mysite/mysite/')
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
