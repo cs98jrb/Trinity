1ab08dd1d79516e5dbac5f445ca3c231a50b74e5
cs98jrb <james@pjshire.me.uk>
2014-12-31 00:25:44 +0000

    T and C

--- mysite/mysite/views/tandc.py
+++ mysite/mysite/views/tandc.py
@@ -2,7 +2,7 @@ __author__ = 'james'
 from django.utils import timezone
 from django.views import generic
 from django.http import HttpResponse
-from django.templatetags.static import static
+from django.conf import settings
 
 from django.shortcuts import render
 
@@ -10,14 +10,14 @@ from events.models import Event
 
 
 def tandc(request):
-    with open('static/mysite/files/terms-and-conditions.pdf', 'r') as pdf:
+    with open(settings.BASE_DIR+'/static/mysite/files/terms-and-conditions.pdf', 'r') as pdf:
         response = HttpResponse(pdf.read(), content_type='application/pdf')
         response['Content-Disposition'] = 'inline;filename=terms-and-conditions.pdf'
         return response
     pdf.closed
 
 def privacy_policy(request):
-    with open('static/mysite/files/privacy-policy.pdf', 'r') as pdf:
+    with open(settings.BASE_DIR+'/static/mysite/files/privacy-policy.pdf', 'r') as pdf:
         response = HttpResponse(pdf.read(), content_type='application/pdf')
         response['Content-Disposition'] = 'inline;filename=terms-and-conditions.pdf'
         return response
